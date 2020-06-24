498b8006a79df6c1877f2d08aa9a27cc364f095c
daihuku99 <dai9huku@gmail.com>
2020-06-24 23:25:52 +0900

    update migration photos/image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_141727) do
+ActiveRecord::Schema.define(version: 2020_06_24_142403) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_06_24_141727) do
 
   create_table "photos", force: :cascade do |t|
     t.integer "user_id"
-    t.string "image_id"
+    t.string "image"
     t.string "spot"
     t.string "address_city"
     t.string "address_street"
